7d620b8e653f6a2f41dffd14107bdc4bf552a67f
Aaron Leo Cooper <aaronleocooper@gmail.com>
2017-07-31 22:42:03 +0800

    Added mocha config and mocha-typescript

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,29 @@
-import 'mocha';
+import { suite, test } from 'mocha-typescript';
 import { assert } from 'chai';
 import { stub } from 'sinon';
 
-import {
-  app
-} from './index';
+import { app } from './index';
 
 suite('app', () => {
 
   test('#feedMe()', () => {
-    const log = stub('console.log');
+    const log = stub(console, 'log');
 
     app.feedMe('test');
 
     assert.isTrue(log.calledWith('fed test !'), 'calls console.log with correct value');
+
+    log.restore();
+  });
+
+  test('.shutUp()', () => {
+    const log = stub(console, 'log');
+
+    // app.shutUp();
+
+
+
+    log.restore();
   });
 
 });
